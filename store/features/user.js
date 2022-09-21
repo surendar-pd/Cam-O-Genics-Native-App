d71f0b52b6181d8a73392f9b25b4d40bc5bc79c3
Kunal Keshan <kunalkeshan12@gmail.com>
2022-09-21 15:55:53 +0530

    added redux functions for user

--- store/features/user.js
+++ store/features/user.js
@@ -4,29 +4,55 @@
 
 // Dependencies
 import { createSlice } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const initialState = {
-    fullName: null,
-    officialEmail: null,
-    cogcId: null,
-    avatar: null,
-    defaultAvatar: null,
-    settings: {
-        email: {}
-    }
+    user: {
+        fullName: null,
+        officialEmail: null,
+        cogcId: null,
+        avatar: null,
+        defaultAvatar: null,
+        settings: {
+            email: {}
+        }
+    },
 };
+
+const getUserFromLocalStorage = async () => {
+    let user = await AsyncStorage.getItem('cogcUser');
+    user = user ? JSON.parse(user) : initialState.user;
+    initialState.user = user;
+    return Promise.resolve('User state set successfully!');
+};
+
+getUserFromLocalStorage()
+    .then((res) => {
+        console.log(res);
+    })
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
-    reducer: {
-        loginUser: (state, action) => { },
-        logoutUser: (state, action) => { },
+    reducers: {
+        loginUser: (state, action) => {
+            // Action Payload - User Object from backend response
+            state.user = { ...action.payload };
+            AsyncStorage.setItem('cogcUser', JSON.stringify(state.user), () => {
+                console.log('User Logged in!');
+            });
+        },
+        logoutUser: (state, action) => {
+            state.user = initialState.user;
+            AsyncStorage.removeItem('cogcUser', () => {
+                console.log('User Logged out!');
+            });
+        },
     }
 });
 
 // Exporting Actions.
-export const { } = userSlice.actions;
+export const { loginUser, logoutUser } = userSlice.actions;
 
 // Exporting Reducer
 export default userSlice.reducer;
